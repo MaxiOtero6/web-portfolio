eaaf1479d9c620aff2ba5aaa5d549d176c0225f7
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-01-11 15:54:01 -0300

    fix: lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import background from "@/assets/images/background.jpg";
