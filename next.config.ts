90c823adc2281cfd36086a6e0be2a670b284044e
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-01-12 14:43:27 -0300

    build: nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	// basePath: "/web-portfolio",
-	// output: "export",
-	// reactStrictMode: true,
+	output: "export",
+	reactStrictMode: true,
+	images: { unoptimized: true },
+	distDir: "dist",
 };
 
 export default nextConfig;
